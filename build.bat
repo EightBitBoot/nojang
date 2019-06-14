6ae9686325bd5730d4db8909d00ead170c957076
Adin W-T <adinrwt@gmail.com>
2019-06-14 19:32:14 -0400

    Updated build.bat to test for bin\

--- build.bat
+++ build.bat
@@ -3,6 +3,8 @@
 set WARN_DISABLE="/wd4710"
 set DEFINES="/D_CRT_SECURE_NO_WARNINGS"
 
+IF NOT EXIST "bin\" mkdir bin
+
 pushd bin
 echo Compiling:
 cl /nologo /diagnostics:caret /Zi %WARN_DISABLE% %DEFINES% WS2_32.lib ..\nojang.c
