63cac6dd5db8fe08fce325736e40900788275b8a
Adin W-T <adinrwt@gmail.com>
2019-06-16 12:47:16 -0400

    Fixed issue with build.bat using quotes in variables and moved library list to linker arguments

--- build.bat
+++ build.bat
@@ -1,12 +1,13 @@
 @echo off
 
-set WARN_DISABLE="/wd4710"
-set DEFINES="/D_CRT_SECURE_NO_WARNINGS"
+set WARNS_DISABLE=/wd4710
+set DEFINES=/D_CRT_SECURE_NO_WARNINGS
+set LIBRARIES=WS2_32.lib
 
 IF NOT EXIST "bin\" mkdir bin
 
 pushd bin
 echo Compiling:
-cl /nologo /diagnostics:caret /Zi %WARN_DISABLE% %DEFINES% WS2_32.lib ..\nojang.c
+cl /nologo /diagnostics:caret /Zi %WARNS_DISABLE% %DEFINES% ..\nojang.c /link %LIBRARIES%
 popd
 
